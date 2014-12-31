4888342e8ef6d951d3fd4999725756296d49be44
Erik Carlsson <info@code301.com>
2014-12-31 16:44:37 +0100

    "Init."

---
+++ lib/Dist/Zilla/MintingProfile/MapMetro/Map.pm
@@ -0,0 +1,28 @@
+package Dist::Zilla::MintingProfile::MapMetro::Map;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::MapMetro::Map - Short intro
+
+=head1 SYNOPSIS
+
+  use Dist::Zilla::MintingProfile::MapMetro::Map;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::MintingProfile::MapMetro::Map is ...
+
+=head1 SEE ALSO
+
+=cut
